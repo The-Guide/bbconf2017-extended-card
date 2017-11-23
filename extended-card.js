82b17ad5ede66f05c3a8c4bdac1c29ba87afb622
Gerjan van Geest <gerjan.van.geest@ing.nl>
2017-11-23 16:16:59 +0100

    fix(chore): Update extended-card to be in line with the codelabs

--- extended-card.js
+++ extended-card.js
@@ -4,23 +4,25 @@ class ExtendedCard extends SimpleCard {
         ${super.styleTemplate}
         <style>
           :host {
-            height: var(--ing-uic-card-height, auto);
-            border-radius: 4px;
+            font-family: Roboto;
           }
-      
-         #footer {
-          padding: calc(var(--spacer-vertical, 8px) * 3) calc(var(--spacer-horizontal, 8px) * 3);
-           border-top: 1px solid var(--divider-color, #d5d5d5);
-           background-color: #f2f2f2;
-           font-size: 0.9em;
-           text-align: center;
-           color: #8c8c8c;
-        }
-      
+
           #header {
             color: orangered;
+            font-size: 1.4em;
           }
-      
+          
+          #footer {
+            border-top: 1px solid #d5d5d5;
+            border-bottom-left-radius: 4px;
+            border-bottom-right-radius: 4px;
+            background-color: #ffeedd;
+            font-size: 1em;
+            text-align: center;
+            color: grey;
+            padding: 4px 24px;
+          }
+          
         </style>
       `;
   }
@@ -29,15 +31,16 @@ class ExtendedCard extends SimpleCard {
       lastUpdated: {
         type: String,
         value: '',
-        reflectToAttribute: 'last-updated',
+        reflectToAttribute: 'last-updated'
       }
     };
   }
+  
   get footerTemplate() {
     if (this.lastUpdated) {
     return html`
     <footer id="footer">
-    Last updated ${this.lastUpdated} mins ago
+      Last updated ${this.lastUpdated} mins ago
     </footer>
     `;}
     else {
@@ -48,16 +51,6 @@ class ExtendedCard extends SimpleCard {
       `;
     }
   }
-  render() {
-    return html`
-      ${this.styleTemplate}
-      ${super.headerTemplate}
-      ${super.bodyTemplate}
-      ${this.footerTemplate}
-    `;
-  }
-
-
 
 }
 
